b3ccd114eea4cfe62a3200ba7ffa21fa309fd8b9
n3kr4s0v4 <evnekrasova@mail.ru>
2022-05-04 16:46:44 +0300

    added exp2

---
+++ week-13/index2.js
@@ -0,0 +1,23 @@
+function sum () {
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert (+a + +b) ;
+}
+
+function sub () {
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert (a - b) ;
+}
+
+function multi () {
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert (a * b) ;
+}
+
+function divis () {
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+    alert (a / b) ;
+}
