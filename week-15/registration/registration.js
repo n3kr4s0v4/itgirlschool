7b715f20010d67ed1459059b26c2fd1eddc98c1b
n3kr4s0v4 <evnekrasova@mail.ru>
2022-08-24 19:50:20 +0300

    added ex2

--- week-15/registration/registration.js
+++ week-15/registration/registration.js
@@ -1,28 +1,27 @@
-function sayHello () {
+function check() {
     let username = document.getElementById ('firstName').value
-    if (username) {
+    let telNum = document.getElementById ('number').value
+    let email = document.getElementById ('mailbox').value
+    let userLastname = document.getElementById ('lastName').value
+    let pass = document.getElementById ('password').value
+    let pass2 = document.getElementById ('password2').value
+
+    document.getElementById ('errorMessage').innerHTML = "" 
+    document.getElementById ('telError').innerHTML = ""
+    document.getElementById ('passError').innerHTML = ""
+
+    if (telNum.length <10 || telNum.length >10) {
+        document.getElementById('telError').innerHTML += "Неверно заполнен номер телефона<br>"
+    }
+
+    if (pass != pass2) {
+        document.getElementById('passError').innerHTML += "Пароли не совпадают<br>"
+    }
+
+    if (username && email && telNum && userLastname && pass && pass2) {
         alert ('Добро пожаловать, ' + username + '!')
-    }
-    else {
-        alert ('Не все поля заполнены!')
-    }
 }
-function checkTel() {
-    let telNum = document.getElementById ('number')
-    let email = document.getElementById ('mailbox')
-
-    document.getElementById ('Error').innerHTML = "" 
-    if (email.value == '') {
-        document.getElementById('Error').innerHTML += "Ваш имейл не заполнен"
-    }
-
-    if (telNum.value.length <10) {
-        alert ('Неверно указан телефон!')
-    }
-    else if (telNum.value.length >10) {
-        alert ('Неверно указан телефон!')
-    }
     else {
-        alert ('Не все поля заполнены!')
+        document.getElementById('errorMessage').innerHTML += "Поля не заполнены<br>"
     }
 }
