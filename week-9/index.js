619aff4fb4544290ae9b1cae27de6dc39bd4d0c9
n3kr4s0v4 <84867701+n3kr4s0v4@users.noreply.github.com>
2023-07-16 13:54:01 +0300

    added week-9

---
+++ week-9/index.js
@@ -0,0 +1,16 @@
+let time ;
+console.log (time);
+
+time = 34;
+console.log (time);
+
+console.log ('Старт поездки. Осталось минут: ' + time);
+
+time = 34 - 15;
+
+console.log ('Немного сторис в соцсетях. Осталось минут: ' + time);
+
+console.log ('Немного не новостей, но событий. Осталось минут: ' + `${time - 10}`); 
+
+time = 0;
+console.log ('Вы приехали. Добро пожаловать в Москву');
