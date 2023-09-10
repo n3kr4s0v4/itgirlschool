b8a6b75b5bfabb976055fd8f2857c7afcbcdf112
n3kr4s0v4 <84867701+n3kr4s0v4@users.noreply.github.com>
2023-09-10 20:11:27 +0300

    added changes

--- week-13/новое дз/index.js
+++ week-13/новое дз/index.js
@@ -4,11 +4,12 @@ const textDays = document.querySelector('.day__text');
 
 function countDays () {
     let dayDeclain;
+    let futureYear;
     const days = new Date(dayInput.value);
     const today = new Date();
     const daysTimestamp = +days;
     const todayTimestamp = +today;
-    let diffDays = Math.abs(Math.floor((todayTimestamp - daysTimestamp) / (1000 * 60 * 60 *24)));
+    const diffDays = Math.abs(Math.floor((todayTimestamp - daysTimestamp) / (1000 * 60 * 60 *24)));
     const lastNum = diffDays % 10;
     const lastTwoNums = diffDays % 100;
     const currentDay = days.getDate();
@@ -17,11 +18,26 @@ function countDays () {
     const todayDay = today.getDate();
     const todayMonth = today.getMonth();
     const todayYear = today.getFullYear();
+    futureYear = +(today.setFullYear(today.getFullYear() + 1));
+    const futureDate = Math.abs(Math.floor((futureYear - daysTimestamp) / (1000 * 60 * 60 *24)));
+    const secondLastNum = futureDate % 10;
+    const secondlastTwoNums = futureDate % 100;
+
 
     if (lastNum === 1 && lastTwoNums !== 11) {
         dayDeclain = " день";
     }
     else if (lastNum >= 2 && lastNum <= 4 && (lastTwoNums < 12 || lastTwoNums > 14)) {
+        dayDeclain = " дня";
+    }
+    else {
+        dayDeclain = " дней";
+    }
+
+    if (secondLastNum === 1 && secondlastTwoNums !== 11) {
+        dayDeclain = " день";
+    }
+    else if (secondLastNum >= 2 && secondLastNum <= 4 && (secondlastTwoNums < 12 || secondlastTwoNums > 14)) {
         dayDeclain = " дня";
     }
     else {
@@ -35,7 +51,7 @@ function countDays () {
         textDays.textContent = "С днём рождения!";
     }
     else if (days < today) {
-        textDays.textContent = `День рождения был ${diffDays} ${dayDeclain} назад`;
+        textDays.textContent = `До дня рождения ${futureDate} ${dayDeclain}`;
     }
     else if(days > today) {
         textDays.textContent =`До дня рождения ${diffDays} ${dayDeclain}`;
